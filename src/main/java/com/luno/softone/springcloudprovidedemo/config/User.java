5ded54706cc643ea8e6d68dac09000588783fd54
unknown <lsh13020@baozun.cn>
2018-12-19 15:54:41 +0800

    first commit

---
+++ src/main/java/com/luno/softone/springcloudprovidedemo/config/User.java
@@ -0,0 +1,15 @@
+package com.luno.softone.springcloudprovidedemo.config;
+
+/**
+ * @author luojian
+ * @version 1.0
+ * @ClassName: User
+ * @Reason: TODO ADD REASON(可选)
+ * @date: 2018年12月13日 13:58
+ * @company:宝尊电商
+ * @since JDK 1.8
+ */
+public class User {
+
+
+}
---
+++ src/test/java/com/luno/softone/springcloudprovidedemo/SpringCloudProvideDemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.luno.softone.springcloudprovidedemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudProvideDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
